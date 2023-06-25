e9b3dca8871a63e68db20502b26790d5823babae
Le Tan Vy <letanvy020799er@gmail.com>
2023-06-25 20:46:38 +0700

    Create entity, Dtos, Repository pattern for Product Microservices

---
+++ src/Services/Product.API/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Product.API.Extensions
+{
+    public static class ApplicationExtensions
+    {
+        public static void UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API"));
+
+            app.UseRouting();
+            // app.UseHttpsRedirection(); //for production only
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
---
+++ src/Services/Product.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Contracts.Common.Interfaces;
+using Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using Product.API.Persistence;
+using Product.API.Repositories;
+using Product.API.Repositories.Interfaces;
+
+namespace Product.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.Configure<RouteOptions>(opt => opt.LowercaseUrls = true);
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.ConfigureProductDbContext(configuration);
+            services.AddInsfrastructureServices();
+
+            return services;
+        }
+
+        private static IServiceCollection ConfigureProductDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnectionString");
+            var builder = new MySqlConnectionStringBuilder(connectionString);
+
+            services.AddDbContext<ProductContext>(m => m.UseMySql(builder.ConnectionString,
+                ServerVersion.AutoDetect(builder.ConnectionString), e =>
+                {
+                    e.MigrationsAssembly("Product.API");
+                    e.SchemaBehavior(MySqlSchemaBehavior.Ignore);
+                }));
+
+            return services;
+        }
+
+        private static IServiceCollection AddInsfrastructureServices(this IServiceCollection services)
+        {
+            return services.AddScoped(typeof(IRepositoryBaseAsync<,,>), typeof(RepositoryBaseAsync<,,>))
+                .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+                .AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
